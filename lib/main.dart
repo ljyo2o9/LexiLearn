33e0a6b231dbbba4f33d495c8b51d935aef8c07a
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-27 15:20:57 +0900

    main.dart 수정
    bookmark 관련 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:lexi_learn/data/model/bookmark_model.dart';
 import 'package:provider/provider.dart';
 import 'package:go_router/go_router.dart';
 
 import 'package:lexi_learn/ui/view_model/finding_words_view_model.dart';
 import 'package:lexi_learn/ui/view_model/finding_wiki_view_model.dart';
+import 'package:lexi_learn/ui/view_model/bookmark_view_model.dart';
 import 'package:lexi_learn/ui/view_model/widget_set_state.dart';
 
 import 'package:lexi_learn/ui/view/main_page.dart';
 import 'package:lexi_learn/ui/view/finding_words_page.dart';
 import 'package:lexi_learn/ui/view/finding_wiki_page.dart';
+import 'package:lexi_learn/ui/view/bookmark_page.dart';
 
 final GoRouter _router = GoRouter(
   initialLocation: '/',
@@ -25,16 +29,26 @@ final GoRouter _router = GoRouter(
     GoRoute(
       path: '/findingWiki',
       builder: (context, state) => const FindingWikiPage(),
+    ),
+    GoRoute(
+      path: '/bookmark',
+      builder: (context, state) => const BookMarkPage(),
     )
   ],
 );
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(BookMarkModelAdapter());
+
+  await Hive.openBox<BookMarkModel>('bookmark');
+
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => FindingWordsViewModel()),
         ChangeNotifierProvider(create: (context) => FindingWikiViewModel()),
+        ChangeNotifierProvider(create: (context) => BookMarkViewModel()),
         ChangeNotifierProvider(create: (context) => WidgetSetState()),
       ],
       child: const MyApp(),
