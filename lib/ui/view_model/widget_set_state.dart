33f2fdc65fc78e736d4475234eb258ac480347b5
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-27 15:19:57 +0900

    widget_set_state.dart 수정
    바로가기 생성 페이지 버튼 설정

--- lib/ui/view_model/widget_set_state.dart
+++ lib/ui/view_model/widget_set_state.dart
@@ -1,44 +1,23 @@
 import 'package:flutter/material.dart';
 
 class WidgetSetState extends ChangeNotifier {
-  int _cnt = 0;
-  int _cnt2 = 0;
+  int _cnt = 1;
 
   int get cnt => _cnt;
 
-  int get cnt2 => _cnt2;
-
-  toggle() {
-    if (_cnt == 0) {
-      _cnt = 1;
-    } else if (_cnt == 1) {
-      _cnt = 0;
-    }
-
+  selectWord() {
+    _cnt = 1;
     notifyListeners();
   }
 
-  toggle2() {
-    if (_cnt2 == 0) {
-      _cnt2 = 1;
-    } else if (_cnt2 == 1) {
-      _cnt2 = 0;
-    }
-
+  selectWiki() {
+    _cnt = 0;
     notifyListeners();
   }
 
   onlyZero() {
-    if (_cnt == 1) {
-      _cnt = 0;
-    }
-
-    notifyListeners();
-  }
-
-  onlyZero2() {
-    if (_cnt2 == 1) {
-      _cnt2 = 0;
+    if (_cnt == 0) {
+      _cnt = 1;
     }
 
     notifyListeners();
